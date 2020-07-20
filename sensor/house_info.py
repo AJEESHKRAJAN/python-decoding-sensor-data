36ce2b623f55cccc3a863fb48abe6882bf362b44
Ajeesh K Rajan <AJEESHKRAJAN@users.noreply.github.com>
2020-07-20 12:00:16 +0100

    Home Data Class

---
+++ sensor/house_info.py
@@ -0,0 +1,12 @@
+class HouseInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area=0):
+        field_data = []
+        for record in self.data:
+            if (record[field] == 0):
+                field_data = record[field]
+            elif rec_area == int(record["area"]):
+                field_data = record[field]
+        return field_data
